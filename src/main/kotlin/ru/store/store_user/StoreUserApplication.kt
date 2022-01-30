6228c77825311f8c93e407106c16cbb554eab1ec
KirillBelyaev74 <kirbel74@gmail.com>
2022-01-30 15:12:51 +0500

    create project

---
+++ src/main/kotlin/ru/store/store_user/StoreUserApplication.kt
@@ -0,0 +1,11 @@
+package ru.store.store_user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StoreUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<StoreUserApplication>(*args)
+}
---
+++ src/test/kotlin/ru/store/store_user/StoreUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.store.store_user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StoreUserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
